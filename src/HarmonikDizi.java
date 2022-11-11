facc4e563fcb048e7cfad777bfad797211de7619
muratAVC <110787002+muratAVC@users.noreply.github.com>
2022-11-11 22:48:49 +0300

    zombiler

--- src/HarmonikDizi.java
+++ src/HarmonikDizi.java
@@ -5,13 +5,12 @@ public class HarmonikDizi {
         Scanner girdi= new Scanner(System.in);
         System.out.println("Harmonik dizisi hesaplanacak sayiyi giriniz");
         int dizi=girdi.nextInt();
-        double toplam=0.0;
 
+        double toplam=0.0;
         for (double i = 1; i <= dizi ; i++) {
             toplam+=(1.0/i);
-
-
         }
+
         System.out.println(toplam);
 
     }
